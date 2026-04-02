798a758b79d8ab907f2db2716b435787c3a30cac
aleksei-sapozhnikov <vermucht@gmail.com>
2026-04-03 00:27:23 +0200

    Clarify primary contact label in actor modal
    # Conflicts:
    #	services-core/aggregator-ui/src/components/ActorModal.tsx

--- services-core/aggregator-ui/src/components/ActorModal.tsx
+++ services-core/aggregator-ui/src/components/ActorModal.tsx
@@ -95,9 +95,7 @@ export default function ActorModal({
                     }}
                   >
                     {primaryContact?.id === contact.id && (
-                      <span className="actor-modal-contact-prefix">
-                        Primary
-                      </span>
+                      <span className="actor-modal-contact-prefix">Primary</span>
                     )}
                     <span className="contact-icon-label">
                       <span
